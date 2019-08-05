50dc139ac50808a19cd5f0389f596a4f831a3251
xiam <xiam@gzsendi.cn>
2019-08-05 10:42:33 +0800

    java测试

---
+++ src/com/jiaozi/hello/Hello.java
@@ -0,0 +1,12 @@
+package com.jiaozi.hello;
+
+/**
+ * @Description: 测试
+ * @Author: xiam
+ * @Create: 2019-08-05
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello...");
+    }
+}
